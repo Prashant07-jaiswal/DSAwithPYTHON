c7e110281e0056934ff36f4e1dfcedb27e058064
Prashant Jaiswal <jaiswalprashant002@gmail.com>
2026-06-20 03:45:11 +0530

    correction

--- Day01_array/Reverse_Traversal.py
+++ Day01_array/Reverse_Traversal.py
@@ -2,10 +2,12 @@ from array import *
 
 #this is Reverse Traversal algorithen concept
 def findleader(arr):
+
     #n stores size of array
     n = len(arr)
     if n == 0:
         return[]
+    
     #max_so_far contain negative infinite value which always less than zero 
     #We use negative infinity as a safe "starting point" when we are trying to find the maximum value in a list of numbers.
     max_so_far = float('-inf')
@@ -23,22 +25,22 @@ def findleader(arr):
 print(findleader([16,17,4,3,5,2]))
 
 
-from numpy import *
+# from numpy import *
 
-def find_leader(arr):
-    n = len(arr)
-    if n==0:
-        return []
-    leader_element = float(-1)
-    leader = array([])
-    tem_list =[]
-    for i in range(n-1,-1,-1):
-        if arr[i]>leader_element:
-            leader_element = arr[i]
-            tem_list.append(arr[i])
-            new_learder = append(leader,tem_list)
+# def find_leader(arr):
+#     n = len(arr)
+#     if n==0:
+#         return []
+#     leader_element = float(-1)
+#     leader = array([])
+#     tem_list =[]
+#     for i in range(n-1,-1,-1):
+#         if arr[i]>leader_element:
+#             leader_element = arr[i]
+#             tem_list.append(arr[i])
+#             new_learder = append(leader,tem_list)
     
-    reversed_leader = flip(new_learder)
-    return reversed_leader
+#     reversed_leader = flip(new_learder)
+#     return reversed_leader
 
-print(find_leader([16,17,4,3,5,2]))
+# print(find_leader([16,17,4,3,5,2]))
