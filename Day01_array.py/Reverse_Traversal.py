d4c16ddb19d9c7f0a1a42039a55d59012a578d55
Prashant Jaiswal <jaiswalprashant002@gmail.com>
2026-06-19 15:37:54 +0530

    Update Reverse_Traversal.py

--- Day01_array.py/Reverse_Traversal.py
+++ Day01_array.py/Reverse_Traversal.py
@@ -22,3 +22,23 @@ def findleader(arr):
 
 print(findleader([16,17,4,3,5,2]))
 
+
+from numpy import *
+
+def find_leader(arr):
+    n = len(arr)
+    if n==0:
+        return []
+    leader_element = float(-1)
+    leader = array([])
+    tem_list =[]
+    for i in range(n-1,-1,-1):
+        if arr[i]>leader_element:
+            leader_element = arr[i]
+            tem_list.append(arr[i])
+            new_learder = append(leader,tem_list)
+    
+    reversed_leader = flip(new_learder)
+    return reversed_leader
+
+print(find_leader([16,17,4,3,5,2]))
