0fc52c7d3b064bf94f9ff5a9a872f84ba84cd596
Prashant Jaiswal <jaiswalprashant002@gmail.com>
2026-06-20 13:29:34 +0530

    Update singly_linklist.py

--- Day_02_Singly_Linklist/singly_linklist.py
+++ Day_02_Singly_Linklist/singly_linklist.py
@@ -44,35 +44,71 @@ class Singlylinklist:
             self.head = temp
     
     
+    #Insert node at the beginning 
     def insertAtBeg(self,value):
+
+        #create Node and temp point on it
         temp = Node(value)
+
+        #store address of head in t1.next block
         temp.next = self.head
+
+        #now head variable pointing on the newly created node as it added to the beginning of th LL
         self.head = temp
 
+    #Insrt a node at the middle of LL
+    #"x" parameter  id taken as after which node newly created node to be added
     def insertATMid(self,value,x):
         temp = Node(value)
         t1 = self.head
         while (t1.next != None):
+
+            #checks if the t1 which is iterated a node is same to node which we are finding 
             if (t1.data == x):
+
+                #firtly link the next node to newly created(temp) by storing address of next node in temp.next block
                 temp.next = t1.next
+
+                #second store newly created node in current t1 node 
                 t1.next = temp
+            
+            #this allow next iteration on node
             t1 = t1.next
 
+    #for deletion of node from LL
     def deleteLL(self,value):
         t1 = self.head
+
+        #this point the previous node of t1 currently iterate
         prev = t1
+
+        #if we want to delete first node 
         if (t1.data == value):
+
+            #head point the next node whoes address stored in t1.next
             self.head = t1.next
         while (t1.next != None):
+
+            #checks for the middle 
             if (t1.data == value):
+
+                #link with the next node of deletion one which is t1
                 prev.next = t1.next
                 break
+
+            #if not found 
             else:
+                # this point the previous node of t1 currently iterate
                 prev = t1
                 t1 = t1.next
+        
+        #if we want to delete last node 
         if (t1.data == value):
+
+            #make previous node last by storing None at next block
             prev.next = None
 
+    #this print all the node of LL
     def printall(self):
         t1 = self.head
         while (t1.next != None):
